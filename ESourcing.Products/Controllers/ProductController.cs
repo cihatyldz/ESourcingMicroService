99c2a9810842e1517ed60ac494cbaecb1c26bf1b
cihat yıldız <cihatyldz1907@gmail.com>
2024-01-12 22:00:56 +0300

    Add project files.

---
+++ ESourcing.Products/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESourcing.Products.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+
+
+    }
+}
---
+++ ESourcing.Products/Settings/ProductDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace ESourcing.Products.Settings
+{
+    public class ProductDatabaseSettings : IProductDatabaseSettings
+    {
+        public string ConnectionStrings { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string CollectionName { get; set; }
+    }
+}
